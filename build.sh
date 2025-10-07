3d809f2b7b089c5f38449903989e3453f38ef223
D4r3N <enzo.suares@icloud.com>
2025-10-07 10:01:25 +0200

    create build scripts

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+# set -e
+
+MODULES=("apn" "camera" "gamecenter" "inappstore" "photo_picker")
+MOD="release" # <release|debug|release_debug>
+VERSION="4.0" # <3.0|4.0>
+
+for MODULE in "${MODULES[@]}"; do	# Compile static libraries
+	# ARM64 Device
+	scons target=$MOD arch=arm64 plugin=$MODULE version=$VERSION
+
+	# Creating a xcframework
+	xcodebuild -create-xcframework -library "./bin/lib$MODULE.arm64-ios.$MOD.a" -output "./bin/$MODULE.$MOD.xcframework"
+
+	# Cleaning
+	rm ./bin/lib*.a
+done
---
+++ build_including_simulator.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+# set -e
+#
+MODULES=("apn" "camera" "gamecenter" "inappstore" "photo_picker")
+MOD="release_debug" #<release|debug|release_debug>
+VERSION="4.0" #<3.0|4.0>
+
+for MODULE in "${MODULES[@]}"; do	# Compile static libraries
+	# ARM64 Device
+	scons target=$MOD arch=arm64 plugin=$MODULE version=$VERSION
+	# x86_64 Simulator
+	scons target=$MOD arch=x86_64 simulator=yes plugin=$MODULE version=$VERSION
+	# ARM64 Simulator
+	scons target=$MOD arch=arm64 simulator=yes plugin=$MODULE version=$VERSION
+
+	# Creating a fat libraries for device and simulator
+	lipo -create \
+		"./bin/lib$MODULE.x86_64-simulator.$MOD.a" \
+		"./bin/lib$MODULE.arm64-simulator.$MOD.a" \
+		-output "./bin/lib$MODULE-simulator.$MOD.a"
+
+	# Creating a xcframework
+	xcodebuild -create-xcframework \
+	    -library "./bin/lib$MODULE.arm64-ios.$MOD.a" \
+	    -library "./bin/lib$MODULE-simulator.$MOD.a" \
+	    -output "./bin/$MODULE.$MOD.xcframework"
+
+	# Cleaning
+	rm ./bin/lib*.a
+done
