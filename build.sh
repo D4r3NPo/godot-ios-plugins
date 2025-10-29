3dd0a7247109a4e7422b43113f646e14498a2978
D4r3N <enzo.suares@icloud.com>
2025-10-29 10:50:16 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,21 @@ MODULES=("apn" "gamecenter" "inappstore" "photo_picker")
 MOD="debug" # <release|debug|release_debug>
 VERSION="4.0" # <3.0|4.0>
 
+rm -r bin/*
+
+for MODULE in "${MODULES[@]}"; do	# Compile static libraries
+	# ARM64 Device
+	scons target=$MOD arch=arm64 plugin=$MODULE version=$VERSION
+
+	# Creating a xcframework
+	xcodebuild -create-xcframework -library "./bin/lib$MODULE.arm64-ios.$MOD.a" -output "./bin/$MODULE.$MOD.xcframework"
+
+	# Cleaning
+	rm ./bin/lib*.a
+done
+
+MOD="release"
+
 for MODULE in "${MODULES[@]}"; do	# Compile static libraries
 	# ARM64 Device
 	scons target=$MOD arch=arm64 plugin=$MODULE version=$VERSION
